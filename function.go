651dc0bcca20edeef0360ad7bbdf0d7e90ce56c0
Lê Hùng Thiện <thienlhse61374@fpt.edu.vn>
2019-01-17 12:49:41 +0700

    Add slack api library

--- function.go
+++ function.go
@@ -6,6 +6,9 @@ import (
 	"fmt"
 	"html"
 	"net/http"
+	"os"
+
+	"github.com/nlopes/slack"
 )
 
 // HelloWorld prints the JSON encoded "message" field in the body
@@ -13,14 +16,27 @@ import (
 func HelloWorld(w http.ResponseWriter, r *http.Request) {
 	var d struct {
 		Message string `json:"message"`
+		User    string `json:"user"`
 	}
+
 	if err := json.NewDecoder(r.Body).Decode(&d); err != nil {
 		fmt.Fprint(w, "Hello World!")
 		return
 	}
-	if d.Message == "" {
+
+	if d.User == "" {
 		fmt.Fprint(w, "Hello World!")
 		return
 	}
+
 	fmt.Fprint(w, html.EscapeString(d.Message))
+
+	api := slack.New(os.Getenv("SLACK_TOKEN"))
+	user, err := api.GetUserInfo(d.User)
+	if err != nil {
+		fmt.Printf("%s\n", err)
+		return
+	}
+
+	fmt.Printf("ID: %s, Fullname: %s, Email: %s\n", user.ID, user.Profile.RealName, user.Profile.Email)
 }
--- go.mod
+++ go.mod
@@ -1,1 +1,8 @@
 module cloudfunction
+
+require (
+	github.com/gorilla/websocket v1.4.0 // indirect
+	github.com/lusis/slack-test v0.0.0-20180109053238-3c758769bfa6 // indirect
+	github.com/nlopes/slack v0.4.0
+	github.com/pkg/errors v0.8.1 // indirect
+)
