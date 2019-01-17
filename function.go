fbda21657a96162c3bbefd7bbe705085a2d11ff4
Lê Hùng Thiện <thienlhse61374@fpt.edu.vn>
2019-01-17 17:19:43 +0700

    Update to response to Slack challenge

--- function.go
+++ function.go
@@ -4,39 +4,59 @@ package p
 import (
 	"encoding/json"
 	"fmt"
-	"html"
 	"net/http"
 	"os"
-
-	"github.com/nlopes/slack"
 )
+
+// SlackToken Authentication token from slack
+var SlackToken = os.Getenv("SLACK_TOKEN")
+
+// VerificationToken Verification token from slack
+var VerificationToken = os.Getenv("VERIFICATION_TOKEN")
 
 // HelloWorld prints the JSON encoded "message" field in the body
 // of the request or "Hello, World!" if there isn't one.
 func HelloWorld(w http.ResponseWriter, r *http.Request) {
-	var d struct {
-		Message string `json:"message"`
-		User    string `json:"user"`
+	var event struct {
+		Token     string `json:"token"`
+		Challenge string `json:"challenge"`
 	}
 
-	if err := json.NewDecoder(r.Body).Decode(&d); err != nil {
-		fmt.Fprint(w, "Hello World!")
+	if err := json.NewDecoder(r.Body).Decode(&event); err != nil {
+		fmt.Fprint(w, "Invalid input")
 		return
 	}
 
-	if d.User == "" {
-		fmt.Fprint(w, "Hello World!")
+	if !verifyToken(event.Token) {
+		fmt.Fprint(w, "Invalid token")
 		return
 	}
 
-	fmt.Fprint(w, html.EscapeString(d.Message))
-
-	api := slack.New(os.Getenv("SLACK_TOKEN"))
-	user, err := api.GetUserInfo(d.User)
-	if err != nil {
-		fmt.Fprintf(w, "%s", err)
+	if event.Challenge != "" {
+		// Respond to Slack event subscription URL verification challenge
+		fmt.Fprintf(w, "{'challenge': %s}", event.Challenge)
 		return
 	}
 
-	fmt.Fprintf(w, "ID: %s, Fullname: %s, Email: %s", user.ID, user.Profile.RealName, user.Profile.Email)
+	// if d.User == "" {
+	// 	fmt.Fprint(w, "Hello World!")
+	// 	return
+	// }
+
+	// api := slack.New(SlackToken)
+	// user, err := api.GetUserInfo(d.User)
+	// if err != nil {
+	// 	fmt.Fprintf(w, "%s", err)
+	// 	return
+	// }
+
+	// fmt.Fprintf(w, "ID: %s, Fullname: %s, Email: %s", user.ID, user.Profile.RealName, user.Profile.Email)
+}
+
+func verifyToken(token string) bool {
+	if token == VerificationToken {
+		return true
+	}
+
+	return false
 }
